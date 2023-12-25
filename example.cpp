ad426c6557259107bf2683dffcee17360045bb85
Christian Rauch <Rauch.Christian@gmx.de>
2023-12-25 02:12:28 +0100

    show some information

--- example.cpp
+++ example.cpp
@@ -4,4 +4,9 @@
 int main() {
   torch::Tensor tensor = torch::rand({2, 3});
   std::cout << tensor << std::endl;
+
+  std::cout << "CUDA devices: " << torch::cuda::device_count() << std::endl;
+  std::cout << "config: " << std::endl << at::show_config() << std::endl;
+  std::cout << "flags: " << std::endl << at::get_cxx_flags() << std::endl;
+  std::cout << "cpu capability: " << at::get_cpu_capability() << std::endl;
 }
